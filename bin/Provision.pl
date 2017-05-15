2659598230fcb7a0cd29539d962ed4ed51186f19
Cloud User <ec2-user@ip-172-31-42-180.us-west-2.compute.internal>
2017-05-15 10:11:04 -0400

    Added ssh module and install framework

--- bin/Provision.pl
+++ bin/Provision.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w -I ../Pmodules/
+#!/usr/bin/perl -w -I ../lib
 #-----------------------------------------------------------------
 # Author :	Sayee Iere
 # Description:	This application will accept inputs with specific
@@ -8,30 +8,38 @@
 use strict;
 use warnings;
 use core;
-#use Data::Dumper;
+use Data::Dumper;
 use JSON;
 
+my $log = Log::Log4perl->get_logger("");
 usage () if @ARGV<1;
-
-print "Welcome to provisioner, your json file will be parsed and validated now \n";
-sleep 2;
+$log->info("Your json file will be parsed and validated now");
 
 # This is a reference to the hash that holds the parsed json file
 my $Arg;
-die "Error" unless json_parse();
+json_parse();
 
 # Initializing the Configuration management object
 my $cm=new core($Arg);
 
+$log->info("Parsing complete, validation in progress");
 $cm->validate($Arg);
+$log->logdie("No supported operations identified, please specify supported methods in json") if (!defined $Arg->{operations});
+$cm->sort_by_priority($Arg);
+
+$log->info("Executing selected operations:");
+foreach my $ops (@{$Arg->{operations}})
+{
+	$cm->$ops($Arg->{$ops}); # Passing the reference to the hash for the chosen operation
+}
+
 
 sub json_parse
 {
 	local $/;
-        open( my $fh, '<', $ARGV[0] );
-        $Arg=decode_json( <$fh> );
-	print "Parsing complete, validation in progress \n";
-	sleep 2;
+        $log->logdie("Unable to locate json file, ensure that the path is correct and readable") unless open( my $fh, '<', $ARGV[0] );
+	$Arg=decode_json(<$fh>);
+	close $fh;
 }
 
 sub usage
--- lib/core.pm
+++ lib/core.pm
@@ -10,17 +10,14 @@ use Exporter;
 @EXPORT_OK = qw();
 
 use strict;
-use File::Find;
-use File::Path;
-use File::Copy;
-use File::Basename;
+use Data::Dumper;
 use Log::Log4perl;
-Log::Log4perl->init("./logger.conf");
+Log::Log4perl->init("../lib/logger.conf"); 	# Module fails to detect local files, hence the relative path
 
-my $SCP   = `which scp`;
-my $SSH   = `which ssh`." -q -o BatchMode=yes";
-my $RSYNC  = `which rsync`;
-my @ERROR = ();
+our $SCP   = `which scp`;
+our $SSH   ="/usr/bin/ssh -q -o BatchMode=yes";
+our $RSYNC  = `which rsync`;
+our $log = Log::Log4perl->get_logger("");
 
 sub new
 {
@@ -30,17 +27,97 @@ sub new
 	return $self;
 }
 
-sub error
-{
-	my $self = shift;
-	return \@ERROR;
-}
-
 sub validate
 {
 	my $self = shift;
-	my $log = Log::Log4perl->get_logger("");
-	$log->error("Test error message");
-	$log->debug("Test debug message");
+	my $Arg = shift;
+	#my $log = Log::Log4perl->get_logger("");
+
+	foreach my $key (keys %{$Arg})
+	{
+		if ($key eq "install"){
+			push(@{$Arg->{operations}}, "install");
+		} elsif ($key eq "remove"){
+			push(@{$Arg->{operations}}, "remove");
+		} elsif ($key eq "configure"){
+			push(@{$Arg->{operations}}, "configure");
+		} elsif ($key eq "service"){
+                        push(@{$Arg->{operations}}, "service");
+		} else { next; }
+	}	
 }
+
+sub sort_by_priority
+{
+	my $self = shift;
+        my $Arg = shift;
+	
+        # Operations identified and validated, now sorting them by priority
+        my @custom_order = qw/ remove install configure service /;
+        my %order = map +($custom_order[$_] => $_), 0 .. $#custom_order;
+	@{$Arg->{operations}}=sort {$order{$a} <=> $order{$b}} @{$Arg->{operations}};
+}
+
+sub install
+{
+        my $self = shift;
+        my $Arg = shift;
+        my @RemoteCommands = ();
+
+        # Looping through the install array to identify modules
+        foreach my $module (keys %{$Arg})
+        {
+		if ($Arg->{$module} eq "auto")
+		{
+                        $log->info("Chosen module: $module, no version specified, choosing automatically");
+                        push(@RemoteCommands, "sudo apt-get install $module -y");
+		} else {
+                	$log->info("Chosen module: $module version: $Arg->{$module}");
+                	push(@RemoteCommands, "sudo apt-get install $module=$Arg->{$module}");
+		}
+        }
+
+	$log->logdie("Failed to complete ssh execution on remote server") if SSHExec("ubuntu","ec2-34-210-183-211.us-west-2.compute.amazonaws.com",\@RemoteCommands);
+}
+
+
+sub remove
+{
+ 	print "In remove operation \n";
+}
+
+sub configure
+{
+	print "In configure operation \n";
+}
+
+sub service
+{
+
+}
+
+sub SSHExec
+{
+	my $ServiceID  = shift;
+	my $Server     = shift;
+	my $Batch      = shift; # Array  reference (list of commands)
+	my @Messages=();
+	my $ExitStatus = shift; # Scalar reference (valid for single command)
+
+	my $RemoteCmd = sprintf("\"%s 2>&1\"",join(" 2>&1;", @{$Batch}));
+	my $Command = "$SSH $ServiceID\@$Server $RemoteCmd";
+	#print $Command;
+	@Messages= `$Command 2>&1`;
+	chomp @Messages;
+	$log->info("@Messages");
+
+	# Experimental : Exit status is valid if batch size is one (ie. one command)
+	#if( ( @{$Batch} eq 1 ) && defined $ExitStatus ){
+	#	$$ExitStatus = int($?/256);
+	#} else {
+	#	$$ExitStatus = undef;
+	#}
+	return;
+}
+
 1;
