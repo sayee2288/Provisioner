b3e0f3aeea99ef45aee36c6dfca84dadf07ea8d1
Ubuntu <ubuntu@ip-172-31-8-169.us-west-2.compute.internal>
2017-05-17 03:11:29 +0000

    updating install script

--- install.sh
+++ install.sh
@@ -1,5 +1,33 @@
-#/bin/bash
-echo "Commencing installation";
+#/usr/bin/bash
+# Bootstrap script to handle dependencies on first time setup
+# Verifying that this master server is ubuntu
+echo "Installing Provisioner";
+
+if sudo apt-get -v &>/dev/null; then
+        echo "Definitely ubuntu, Proceeding with installation";
+else
+        echo "Not an ubuntu system, please run this application on an ubuntu";
+        exit 1;
+fi
+
+if sudo apt-get update &>/dev/null; then
+        echo "Updating apt cache";
+else
+        echo "Failed to updated apt-cache";
+        exit 1;
+fi
+
+if perl -v &>/dev/null; then
+        echo "Perl already exists";
+else
+        echo "Installing perl"
+        if sudo apt-get perl -y &>/dev/null; then
+                echo "Perl installed";
+        else
+                echo "Failed to install Perl";
+                exit 1;
+        fi
+fi
 
 if sudo apt-get install liblog-log4perl-perl -y &>/dev/null; then
 	echo "Installed log4perl";
